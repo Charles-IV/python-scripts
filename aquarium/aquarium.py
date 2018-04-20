4395e04ad0c5880415e112df812e482e861cd5c6
Charles-IV <Charles-IV@users.noreply.github.com>
2018-04-20 17:48:54 +0100

    aquarium: update
    still broken, but here it is
    i don't think fish are even being spawned now, or at least not printed

--- aquarium/aquarium.py
+++ aquarium/aquarium.py
@@ -9,7 +9,7 @@ from cls import cls
 depth = 24
 width = 80
 fish = []
-sprites = ["><>", "><##>", ">#-", ">|||||----"]
+sprites = ["><>", "><##>", ">#-", ">[###]>", "]<@>", ">[>-"]
 
 
 class Fish:
@@ -23,55 +23,67 @@ class Fish:
         self.xPos += 1
 
 
+def getFishOnY(y):
+    fOnY = []  # array with details of the fish on that y
+    for f in fish:
+        if f.yPos == y:  # if the fish is on that row
+            fOnY.append(f)
+
+    return fOnY
+
+
 def draw():
     cls()
     # print top of sea
     for i in range(0, width):
         print("-", end="")
 
-    # NOT THIS BUT ....
-    """
-    pos = []
-    # get yPos's
-    for f in fish:
-        pos.append([f.yPos, f.xPos])
+    # lastFish = [-1]  # set the y position of the last fish printed
+    for i in range(1, depth):  # iterate through the depths
+        """
+        for fi in range(0, len(fish)):  # iterate through the fish
+            if i == fish[fi].yPos:  # if there is a fish on that row
+                if i == lastFish[0]:  # if the lsat fish printed was on that row
+                    counter = lastFish[1] + lastFish[3].length()
+                else:  # if the last fish wasn't on that row
+                    counter = 0
+                for space in range(counter, fish[fi].xPos):  # iterate from the left to the fish
+                    print(" ", end="")  # print a space
+                print(fish[fi].sprite, end="")  # print the fish
+                lastFish = [fish[fi].yPos, fish[fi].xPos, fish[fi].sprite]
 
-    
-    for i in range(0, depth):
-        for p in range(0, len(pos)-1):
-            if i == p[0]:  # if fish is on that row
-                for space in range(0, p[1])
-
-        print()
-"""
-    # THIS ...
-    for i in range(0, depth):  # iterate through the depths
-        for f in range(0, len(fish)-1):  # iterate through the fish
-            if i == fish[f].yPos:  # if there is a fish on that row
-                """
-                for space in range(0, fish[f].xPos):  # iterate from the left to the fish
-                    print(" ")  # print a space
-                # endfor
-                print(fish[f].sprite)  # print the fish
-                """
-                print("true")
             else:  # if there isn't a fish on that row
-                print()  # new row
-            #endif fish is at that depth
-        #endfor f in fish
-    #endfor i in depths
+                print()  # new row"""
 
-    # END THIS STUFF
+        # skip = 0  # number to skip after printing fish
+        fony = getFishOnY(i)  # get the fish on that y
+        if len(fony) == 0:  # if there are no fish on that y
+            print()
+        else:
+            for x in range(0, width):  # go across the page
+                drawn = 0  # how many characters I have drawn on this x
+                for f in fony:
+                    if f.xPos == x + drawn:  # if the fish starts there
+                        print(f.sprite)
+                        drawn += len(f.sprite)
+                        # skip += len(f.sprite) - 1  # currently changing x instead
+                        # x += len(f.sprite)
+
+                if drawn == 0:  # if still haven't drawn sprite
+                    print(" ")  # draw space and go to next x
+
+
+            
 
 
 def spawn():
-    y = random.randint(0, depth)
+    y = random.randint(1, depth)
     sprite = random.randint(0, len(sprites)-1)
-    fish.append(Fish(y, sprite))
+    fish.append(Fish(y, sprites[sprite]))
 
 
 count = 0
-nextSpawn = random.randint(2, 10)
+nextSpawn = random.randint(5, 20)
 
 spawn()
 
@@ -81,9 +93,9 @@ while True:
 
     draw()
 
-    #count += 1
+    count += 1
     if count == nextSpawn:
         spawn()
         count = 0
 
-    time.sleep(1)
+    time.sleep(0.5)
