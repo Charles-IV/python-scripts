fc64654ac3d3484b076e1384dc4e99a7dc7b082d
Charles-IV <Charles-IV@users.noreply.github.com>
2018-04-24 16:11:55 +0100

    aquarium: cleanup and fix problem
    it works!

--- aquarium/aquarium.py
+++ aquarium/aquarium.py
@@ -40,22 +40,6 @@ def draw():
 
     # lastFish = [-1]  # set the y position of the last fish printed
     for i in range(1, depth):  # iterate through the depths
-        """
-        for fi in range(0, len(fish)):  # iterate through the fish
-            if i == fish[fi].yPos:  # if there is a fish on that row
-                if i == lastFish[0]:  # if the lsat fish printed was on that row
-                    counter = lastFish[1] + lastFish[3].length()
-                else:  # if the last fish wasn't on that row
-                    counter = 0
-                for space in range(counter, fish[fi].xPos):  # iterate from the left to the fish
-                    print(" ", end="")  # print a space
-                print(fish[fi].sprite, end="")  # print the fish
-                lastFish = [fish[fi].yPos, fish[fi].xPos, fish[fi].sprite]
-
-            else:  # if there isn't a fish on that row
-                print()  # new row"""
-
-        # skip = 0  # number to skip after printing fish
         fony = getFishOnY(i)  # get the fish on that y
         if len(fony) == 0:  # if there are no fish on that y
             print()
@@ -64,16 +48,13 @@ def draw():
                 drawn = 0  # how many characters I have drawn on this x
                 for f in fony:
                     if f.xPos == x + drawn:  # if the fish starts there
-                        print(f.sprite)
+                        print(f.sprite, end="")
                         drawn += len(f.sprite)
-                        # skip += len(f.sprite) - 1  # currently changing x instead
-                        # x += len(f.sprite)
 
                 if drawn == 0:  # if still haven't drawn sprite
-                    print(" ")  # draw space and go to next x
-
-
-            
+                    print(" ", end="")  # draw space and go to next x
+                    
+        print()          
 
 
 def spawn():
