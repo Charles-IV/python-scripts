c989b4b3220e380f7698cdef021757975dc49abd
Charles-IV <Charles-IV@users.noreply.github.com>
2018-04-25 16:28:50 +0100

    aquarium-beta: backwards fish + more

---
+++ aquarium/aquarium-beta.py
@@ -0,0 +1,109 @@
+"""
+Charles IV
+Aquarium
+"""
+
+import random, time
+from cls import cls
+
+depth = 22
+width = 80
+fish = []
+sprites = ["><>", "><##>", ">#-", ">[###]>", "]<@>", ">[>-"]
+backSprites = ["<><", "<##><", "-#<", "<[##]<", "<@>[", "-<]<"]
+
+
+class Fish:
+    def __init__(self, ypos, back, sprite):
+        if not back:  # if left -> right
+            self.yPos = ypos
+            self.xPos = 0
+            self.back = back
+            self.speed = random.randint(1, 4)
+            self.sprite = sprite
+        else:  # if right -> left
+            self.yPos = ypos
+            self.xPos = width
+            self.back = back
+            self.speed = - random.randint(1, 4)
+            self.sprite = sprite
+
+    def up(self):
+        self.xPos += self.speed
+
+
+def getFishOnY(y):
+    fOnY = []  # array with details of the fish on that y
+    for f in fish:
+        if f.yPos == y:  # if the fish is on that row
+            fOnY.append(f)
+
+    return fOnY
+
+
+def draw():
+    cls()
+    # print top of sea
+    for i in range(0, width):
+        print("-", end="")
+
+    # lastFish = [-1]  # set the y position of the last fish printed
+    for i in range(1, depth):  # iterate through the depths
+        drawn = 0  # how many characters I have drawn on this y
+        fony = getFishOnY(i)  # get the fish on that y
+        #if len(fony) == 0:  # if there are no fish on that y
+        if False:
+            print()
+        else:
+            for x in range(0, width):  # go across the page
+                #drawn = 0  # how many characters I have drawn on this x
+                for f in fony:
+                    if f.xPos == x + drawn:  # if the fish starts there
+                        for char in f.sprite:  # iterate through sprite
+                            if x + drawn >= width:
+                                break  # don't draw if it it's going off edge
+                            else:
+                                print(char, end="")
+                                drawn += 1
+                    #break  # so we don't draw two fish in the same position
+
+                if drawn == 0:  # if still haven't drawn sprite
+                    if x + drawn <= width:
+                        print(" ", end="")  # draw space and go to next x
+                    
+        print()        
+
+
+def spawn():
+    y = random.randint(2, depth)
+    backwards = bool(random.getrandbits(1))  # if fish is going from left to right or right to 
+    if backwards:  # if right -> left
+        fish.append(Fish(y, backwards, sprite = random.choice(backSprites)))  # use backwards sprite
+    else:
+        fish.append(Fish(y, backwards, sprite = random.choice(sprites)))
+
+
+count = 0
+nextSpawn = random.randint(5, 10)
+
+spawn()
+
+while True:
+    for f in fish:
+        f.up()
+        # remove fish from array when off screen
+        if not f.back:
+            if f.xPos > width:
+                fish.remove(f)
+        else:
+            if f.xPos < 0:
+                fish.remove(f)
+
+    draw()
+
+    count += 1
+    if count == nextSpawn:
+        spawn()
+        count = 0
+
+    time.sleep(0.1)
