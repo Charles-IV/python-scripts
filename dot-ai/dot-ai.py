981bc9e2860e642c3860127f30396d52655441c0
Charles-IV <cthefourth@gmail.com>
2018-08-22 15:38:36 +0100

    update to work as expected
    now i can do the actual genetic algorithm

--- dot-ai/dot-ai.py
+++ dot-ai/dot-ai.py
@@ -17,8 +17,8 @@ class Dot:
         # 1 - up, 2 - right, 3 - down, 4 - left
         #self.dir = random.randint(1, 4) - set later
         # Position:
-        # 10x10 grid, start co-ordinates at (5, 1), goal at (5, 10)
-        self.pos = [5, 1]
+        # 20x20 grid, start co-ordinates at (10, 1), goal at (10, 20)
+        self.pos = [10, 1]
         ### Genes of the dots: (to be mutated and passed on)
         # something that makes then chose on direction more often?
         # Something that records their previous directions?
@@ -28,20 +28,20 @@ class Dot:
         # self.fitness will be 1/self.steps - so smallest steps is biggest
         # if they're dead - so they stop moving
 
-    def up(self):  # update function
-        if self.pos == [5, 10]:  # if they're done
+    def up(self, genDone):  # update function
+        if self.pos == [10, 20]:  # if they're done
             genDone = True
 
-        elif self.pos != [5, 10]:  # if they havent finished
+        elif self.pos != [10, 20]:  # if they havent finished
             self.dir = random.randint(1, 4)
             if self.dir == 1:  # if up
-                if self.pos[1] < 10:  # if they're not at the top
+                if self.pos[1] < 20:  # if they're not at the top
                     self.pos[1] += 1  # add one to y pos - 1 up
                 else:
                     self.pos [1] -= 1  # go down so they don't go off screen
                     # also don't bother changing direction because I'm lazy and does it matter?
             elif self.dir == 2:  # if right
-                if self.pos[0] < 10:  # if not to far right
+                if self.pos[0] < 20:  # if not to far right
                     self.pos[0] += 1  # add one to x pos - 1 right
                 else:
                     self.pos[0] -= 1  # go left to stay on screen
@@ -60,6 +60,8 @@ class Dot:
             self.log.append(self.dir)
             self.steps += 1
 
+        return genDone
+
 
 def cls():  # function to clear the terminal screen
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -76,14 +78,14 @@ def onLine(line):  # function to find the opbjects on that line
 
 def draw():
     screen = ""  # what will be put onto the console
-    for y in range(10, 1, -1):  # to go from 10 to 1, as 1 is at the bottom
-        line = ""  # what will go onto the line
+    for y in range(20, 1, -1):  # to go from 10 to 1, as 1 is at the bottom
+        line = " "  # what will go onto the line - one space buffer on left
         obs = onLine(y)  # get the objects on the line
-        for x in range(1, 10):  # go through the x positions on the line
-            char = " "  # character to be drawn (will be space if nothing there)
+        for x in range(1, 20):  # go through the x positions on the line
+            char = "-"  # character to be drawn (will be space if nothing there)
 
             # do goal fist so it#s overwritten by dots
-            if y == 10 and x == 5:
+            if y == 20 and x == 10:
                 char = "#"
 
             # go through objects on the line and add them
@@ -114,6 +116,6 @@ for d in dots:  # go through dots
 
 while genDone == False:  # ima gonna need to ^C this stuff
     for d in dots:
-        d.up()
+        genDone = d.up(genDone)  # do genDone stuff so we know when we're done
     draw()
-    time.sleep(1)  # so you can see it, and to give my cpu a rest
+    time.sleep(0.1)  # so you can see it, and to give my cpu a rest
