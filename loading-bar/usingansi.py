bcadaa384ec85f2787827482f81ee573674e769b
Charles-IV <cthefourth@gmail.com>
2018-09-22 12:42:34 +0000

    ignore pycache and add loading bar
    loading bar is currently a load of test programs, I should
    probably tidy them up

---
+++ loading-bar/usingansi.py
@@ -0,0 +1,28 @@
+from ansiescapes import cursorMove
+from sys.stdout import write
+from time import sleep
+
+
+def CreateProgressBar(barLength, max, value, start="[", end="]", block="#", emp=" ", front="", showPct=False):
+    pct = value / max
+
+    # [, block for the percentage times of barLength, front, emp (empty space) for the rest, ], then a buffer and percentage if showPct
+    return "{}{}{}{}{}{}".format(start, block * int(barLength * pct), front, emp * (barLength - int(barLength * pct)), end, "{:6}%".format(round(pct * 100, 2)) if showPct else "")
+
+
+progress = 0.00
+"""
+while progress <= 100.00:
+    print("\r{}".format(CreateProgressBar(50, 100, progress, showPct=True)), end="")
+    # \r is like \n but stays on the same line (replacing it)
+
+    progress += 0.01
+    #sleep(0.01) - you will probably want this on a good comupter
+"""
+
+print(CreateProgressBar(50, 100, progress, showPct=True), end="")
+
+while progress <= 100:
+    write(cursorMove(-(barLength - int(barLength * pct))) + "#")
+    progress += 0.01
+    sleep(0.01)
