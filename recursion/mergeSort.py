3770cc7c65271657db94aa78c02835037b8ca3d4
Charles-IV <cthefourth@gmail.com>
2018-09-27 20:29:27 +0100

    update mergeSort with local
    lol more tests

--- recursion/mergeSort.py
+++ recursion/mergeSort.py
@@ -1,22 +1,25 @@
-def split(arr):
+def split(arr, origarr):
     arr1 = arr[:len(arr)//2]
     arr2 = arr[len(arr)//2:]  # split array
+    # maybe do .find to find what elements to replace?
+    arr = [arr1, arr2]
     if len(arr1) > 1:
         # split again
-        arr1 = split(arr1)
+        arr = split(arr1, arr)
     if len(arr2) > 1:
         # split again
-        arr2 = split(arr2)
+        arr = split(arr2, arr)
     #return [arr1, arr2]  # now how do i return it without multidimensional list/tuples?
-    ret = []
+    """ret = []
     for i in arr1:
         #return i
         ret.append(i)
     for i in arr2:
         #return i
         ret.append(i)
-    return [ret]
+    return [ret]"""
+    return arr#[arr1, arr2]
 
 a = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
-print(split(a))
+print(split(a, a))
