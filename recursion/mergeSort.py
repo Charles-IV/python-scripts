1dff281481518e1d4b46ce689d458d628a382d62
Charles-IV <cthefourth@gmail.com>
2018-09-27 19:45:52 +0100

    new mergeSort recursion idea
    my brain is still melted.
    and this still doesn't work.

--- recursion/mergeSort.py
+++ recursion/mergeSort.py
@@ -1,14 +1,22 @@
 def split(arr):
     arr1 = arr[:len(arr)//2]
-    arr2 = arr[len(arr)//2:]]  # split array
+    arr2 = arr[len(arr)//2:]  # split array
     if len(arr1) > 1:
         # split again
         arr1 = split(arr1)
-    if len(arr1) == 1:
-        return  arr # finish if split up into induvidual blocks
-    else:
+    if len(arr2) > 1:
+        # split again
+        arr2 = split(arr2)
+    #return [arr1, arr2]  # now how do i return it without multidimensional list/tuples?
+    ret = []
+    for i in arr1:
+        #return i
+        ret.append(i)
+    for i in arr2:
+        #return i
+        ret.append(i)
+    return [ret]
 
+a = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
-for i in arr:
-    if len(i) > 1:
-        i = split(i)
+print(split(a))
