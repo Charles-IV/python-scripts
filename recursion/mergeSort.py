b6303844c3b5b421398ea68458ab9e773d8eeb75
Charles-IV <cthefourth@gmail.com>
2018-09-28 09:02:58 +0000

    mergeSort: use second array for output
    friend showed me his version, i modified, then removed the guff from 
    mine and implemented a second array

--- recursion/mergeSort.py
+++ recursion/mergeSort.py
@@ -1,25 +1,19 @@
-def split(arr, origarr):
+def split(arr, out):
     arr1 = arr[:len(arr)//2]
     arr2 = arr[len(arr)//2:]  # split array
-    # maybe do .find to find what elements to replace?
-    arr = [arr1, arr2]
-    if len(arr1) > 1:
+    if len(arr1) == 1:
+        out.append(arr1)
+    elif len(arr1) > 1:
         # split again
-        arr = split(arr1, arr)
+        split(arr1, out)
+    if len(arr2) == 1:
+        out.append(arr2)
     if len(arr2) > 1:
         # split again
-        arr = split(arr2, arr)
-    #return [arr1, arr2]  # now how do i return it without multidimensional list/tuples?
-    """ret = []
-    for i in arr1:
-        #return i
-        ret.append(i)
-    for i in arr2:
-        #return i
-        ret.append(i)
-    return [ret]"""
-    return arr#[arr1, arr2]
+        split(arr2, out)
+
+    return out
 
 a = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
-print(split(a, a))
+print(split(a, []))
