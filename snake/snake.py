bbd1854d81a61e0b62349773fe839cc2a4846226
Charles-IV <cthefourth@gmail.com>
2018-11-24 18:25:18 +0000

    snake: another big update I can't remember what's in this but it's along the lines of: * new and improved draw function that only prints changes * put all runtime variables into an object so they can be reset easily * I now don't have to pass variables around a lot and struggle with where to modify them * other stuff probably

--- snake/snake.py
+++ snake/snake.py
@@ -9,8 +9,8 @@ init()  # for windows
 
 # some vars
 
-width = 40
-height = 40
+width = 20
+height = 20
 fps = 10
 epilepsy = -1 # traust me, you don't want to enable this
               # (-1 for disabled, 0 for colour set 1, 1 for colour set 2)
@@ -32,10 +32,13 @@ class Food:
         self.xPos = random.randint(1, width)
         self.yPos = random.randint(1, height)
         self.pos = str(self.xPos) + "x" + str(self.yPos)  # makes comparing easier
-        for part in snake:
-            if part.pos == self.pos:  # if the snake is where the food just spawned
-                food.up()  # try to change food postition to where the snake isn't
-        self.eaten = False  # maybe inefficient, but it makes regenerating easier
+        try:
+            for part in v.snake:
+                if part.pos == self.pos:  # if the snake is where the food just spawned
+                    self.up()  # try to change food postition to where the snake isn't
+        except NameError:  # if variables haven't been inited yet
+            if self.pos == str(width//2)+"x"+str(height//2):  # make sure to not spawn at snake spawn point
+                self.up()
             
             
 class SnakeHead:
@@ -48,10 +51,10 @@ class SnakeHead:
         self.over = False  # have a variable for this so gaveOver gan be called and the full snake array passed to it, rather than passing it needlessly through the up method.
         
         
-    def up(self, inp):
+    def up(self):
         # get direction
-        self.dir = inp  # TODO: make this more efficient, don't need duplicate variables
-                        # currently I'm using this to compare the current direction to the input, so the player doesn't turn 180 and kill themselves, but it could be made more efficient
+        self.dir = v.inp  # TODO: make this more efficient, don't need duplicate variables
+                          # currently I'm using this to compare the current direction to the input, so the player doesn't turn 180 and kill themselves, but it could be made more efficient
         
         # move
         if self.dir == "w":  # if up
@@ -69,30 +72,26 @@ class SnakeHead:
         self.pos = str(self.xPos) + "x" + str(self.yPos)
             
         # check if colliding with something
-        if self.pos == food.pos:  # if eating some food
-            spawnPart()  # add a part to the snake
-            food.eaten = True  # tell the food it's been eaten to respawn
+        if self.pos == v.food.pos:  # if eating some food
+            spawnPart(v.snake)  # add a part to the snake
+            v.score += 1  # increment score
+            v.food.up()  # respawn food
             
         if (1 > self.xPos or self.xPos > width) or (1 > self.yPos or self.yPos > height):  # if off screen
-            self.over = True  # tell main bit to end game and recreate vars
+            gameOver()  # call game over screen and reset vars
             
-        for part in snake[1:]:  # iterate through the parts on the snake, skipping the head
+        for part in v.snake[1:]:  # iterate through the parts on the snake, skipping the head
             if part.pos == self.pos:  # if you're eating yourself
-                self.over = True
+                gameOver()
                 
                 
 class SnakePart:
     def __init__(self, mummy):
         self.parent = mummy  # mummy!
         self.pos = self.parent.oldPos  # set position to mummys old possition
-        self.xPos, self.yPos = self.pos.split("x")  # set xPos and yPos
-        self.xPox = int(self.xPos)
-        self.yPos = int(self.yPos)
-        self.oldPos = self.pos
-        self.over = self.parent.over  # placeholder to make my life easier
-        # TODO: most of this could probably be moved into  up() and just call up here
+        self.up()  # create other variables
         
-    def up(self, inp):  # take input but ignore it - thats mummys job
+    def up(self):
         # update old position
         self.oldPos = self.pos
         # update current possition to parents old position and update xPos and yPos
@@ -101,7 +100,29 @@ class SnakePart:
         self.xPos = int(self.xPos)  # change to integers
         self.yPos = int(self.yPos)
         
-        self.over = self.parent.over
+
+class runtimeVars:  # object for storing runtime variables
+    def __init__(self):
+        self.reset()
+        
+    def reset(self):  # reset all to 0
+        self.snake = []
+        self.food = Food()
+        self.snake.append(SnakeHead())
+        for i in range(0, 5):  # create 5 parts
+            self.snake = spawnPart(self.snake)
+        self.inp = "d"
+
+        self.score = 0
+        
+        self.bordersDrawn = False
+        
+        # board to only print updates
+        self.board = []
+        for row in range(0, height):
+            self.board.append([])
+            for column in range(0, width):
+                self.board[row].append(Back.RESET)  # background is probably transparent
         
 
 ############
@@ -119,25 +140,32 @@ def gameOver():
         "    | |__| |/ ____ \| |  | | |____  | |__| | \  /  | |____| | \ \ \n"+
         "     \_____/_/    \_\_|  |_|______|  \____/   \/   |______|_|  \_\ \n\n"+
         "   Y O U   D I E D\n\n"+
-        "   Score = {}\n\n".format(score))
+        "   Score = {}\n\n".format(v.score))
     input("   >")  # clear out all the gunk waiting to be inputed so I get y or n next
     restart = input("\033[1A   Do you want to restart? [Y/n]: ")  # ansi escape to go up one line to overwrite last line
     if restart.lower() == "n":
         exit()
+    v.reset()  # reset all variables
+    cls()  # clear screen do this isn't left over later
     # else just return
     
 
 def pause():
-    for y in range((height//4)+2, (3*(height//4))+3):  # print from a quater to three quarters down the board
-    #for y in range(7, 18):  # print on lines 5-15 of board
+    for y in range((height//4)+3, (3*(height//4))+3):  # print from a quater to three quarters down the board
         print("\033[{0};{2}H{1}\033[{0};{3}H{1}".format(y, Back.WHITE+"   ", width-(width//4)+4, width+(width//4)+4)+Back.RESET, end="")
         # print two white lines parallel to each other
         
     print("\033["+str(height+5)+";4HP A U S E D        (press enter to play)", end="")
     input()  # wait until enter is pressed
-    print("\033[K")  # clear line with paused message on
-
-
+    print("\033[1A\033[K")  # move up to line with paused message on and clear it
+    
+    # update board with colours that changed, so they are overwritten properly
+    for y in range(height//4, 3*(height//4)):
+        v.board[y][2*(width//5)] = Back.WHITE
+        v.board[y][2*(width//5)-1] = Back.WHITE
+        v.board[y][3*(width//5)] = Back.WHITE
+        v.board[y][3*(width//5)+1] = Back.WHITE
+        
 # colour customisation functions
 def validateColInput(disp, col):
     colours = [Back.BLACK, Back.RED, Back.GREEN, Back.YELLOW, Back.BLUE, Back.MAGENTA, Back.CYAN, Back.WHITE, Back.RESET]  # available colours
@@ -185,9 +213,7 @@ def customise():  # allow the user to change the colour of the parts
     
     cls()
     print(Back.RESET)
-    print("""
-   Success!
-   Colour for snake: {}\n""".format(dispCols[0]+"  "+Back.RESET)+
+    print("   Success!\n   Colour for snake: {}\n".format(dispCols[0]+"  "+Back.RESET)+
     "   Colour for border: {}\n".format(dispCols[1]+"  "+Back.RESET)+
     "   Colour for food: {}\n".format(dispCols[2]+"  "+Back.RESET)+
     "   Colour for background: {}\n".format(dispCols[3]+"  "+Back.RESET)+
@@ -202,46 +228,65 @@ def cls():  # to clear screen
     # This method works when access to command prompt is blocked
     
 
-def spawnPart():
+def spawnPart(snake):
     snake.append(SnakePart(snake[len(snake)-1]))  # create part with the end of the array as the parent
+    return snake
 
 
 # function to draw to screen
 def draw():
+    # create old board to compare new board
+    oldBoard = []
+    for row in range(0, height):
+        oldBoard.append([])
+        for item in range(0, width):
+            oldBoard[row].append(v.board[row][item])  # only copy literals to avoid pythons stupid linking
+    
+    # update board and console
     message = ""  # custom banner, for use in debugging
-    console = str(message) + "\n"
-    for y in range(0, height + 2):  # 0, height+2 for border
-        ony = []
-        line = "  " + dispCols[1] + "  "  # spacer and border
+    console = str(message)
+    
+    if not v.bordersDrawn:  # if border needs changing
+        # top border
+        # change position, change colour, print line
+        console += "\033[2;3H" + dispCols[1] + "  " * (width+2)
+        
+        # side borders
+        for y in range(3, height+4):
+            console += "\033[{};3H".format(y) + "  " + "\033[{};{}H".format(y, (width*2)+5) + "  "
+            
+        # bottom border
+        console += "\033[{};3H".format(height+3) + "  " * (width+2) + dispCols[3]  # and change colour back
+        v.bordersDrawn = True
+    
+    # draw snake and food, update board
+    for y in range(1, height+1):
+        ony = []  # array of objects on line
+        
         # get stuff on y
-        for part in snake:
-            if part.yPos == y:
+        for part in v.snake:  # iterate through snake
+            if part.yPos == y:  # check if its on the line being drawn
                 ony.append(part)
-                
-        if food.yPos == y:  #  if the food is on y
-            ony.append(food)  # TODO: later, use if type(ony) == Food
+        
+        if v.food.yPos == y:  # check if food is on line
+            ony.append(v.food)
             
         # go across screen
-        for x in range(1, width + 1):
-            if y == 0 or y == height + 1:  # borders
-                char = dispCols[1] + "  "
-            else:
-                char = dispCols[3] + "  "  # character to write in that position
-                for obj in ony:
-                    if obj.xPos == x:  # if it's this position
-                        if type(obj) == Food:  # if it's food
-                            char = dispCols[2] + "  "  # overwrite character to write
+        for x in range(1, width+1):
+            char = dispCols[3]  # colour to write to this position
+            for obj in ony:  # go though objects on line
+                if obj.xPos == x:   # if its in this position
+                    if type(obj) == Food:  # if it's food
+                        char = dispCols[2]  # change colour
+                    else:  # if it's snake
+                        char = dispCols[0]
                         
-                        else:  # if it snake
-                            char = dispCols[0] + "  "
-                        
-            line += char  # write character in this position
-            
-        console += line + dispCols[1] + "  \n" + Back.RESET  # newline and border
-        
-    console += Style.RESET_ALL + "  Score: {}".format(score)  # reset the style for other outputs
-    cls()  # clear screen
-    print(console)  # replace it
+            v.board[y-1][x-1] = char  # update board with what colour is in this position
+            if v.board[y-1][x-1] != oldBoard[y-1][x-1]:  # if the colour of this positino has changed
+                console += "\033[{};{}H{}".format(y+2, ((x+2)*2)-1, v.board[y-1][x-1] + "  ")  # log position as to be overwritten
+    
+    console += Style.RESET_ALL + "\033[{};3HScore: {}".format(height+4, v.score)  # reset the style for other outputs
+    print(console)  # update screen
     
 
 #################  
@@ -266,58 +311,40 @@ if start == "c":
     customise()
 elif start == "e":
     epilepsy = 0
+    
+cls()
 
 # spawn the stuff
-snake = []
-food = Food()
-snake.append(SnakeHead())
-for i in range(0, 5):  # create 5 parts
-    spawnPart()
-inp = "d"
+v = runtimeVars()  # object for storing all variables in
 
-score = 0
 lastTime = time()
 
 while True:
     # get inputs whenever possible
-    if (is_pressed("w") or is_pressed("up")) and snake[0].dir != "s":
-        inp = "w"
-    elif (is_pressed("a") or is_pressed("left")) and snake[0].dir != "d":
-        inp = "a"
-    elif (is_pressed("s") or is_pressed("down")) and snake[0].dir != "w":
-        inp = "s"
-    elif (is_pressed("d") or is_pressed("right")) and snake[0].dir != "a":
-        inp = "d"
+    if (is_pressed("w") or is_pressed("up")) and v.snake[0].dir != "s":
+        v.inp = "w"
+    elif (is_pressed("a") or is_pressed("left")) and v.snake[0].dir != "d":
+        v.inp = "a"
+    elif (is_pressed("s") or is_pressed("down")) and v.snake[0].dir != "w":
+        v.inp = "s"
+    elif (is_pressed("d") or is_pressed("right")) and v.snake[0].dir != "a":
+        v.inp = "d"
     elif is_pressed("p") or is_pressed("space"):
         pause()
         
     # update everything
     if time() - lastTime >= 1/fps:  # only update if the correct amount of time has elapsed
-        for part in snake:
-            part.up(inp)  # parts that aren't the head will ignore inp
-            if part.over:  # if game over
-                # reset the variables because gameOver can't
-                snake = []
-                snake.append(SnakeHead())
-                for i in range(0, 5):  # create 5 parts
-                    spawnPart()
-                food = Food()
-                inp = "d"
-                gameOver()  # do the gameOver screen
-                score = 0  # reset score afterwards so people can see it on the gameOver screen
-                break  # break out of loop of all the parts
-            
-        if food.eaten:  # if the food's been eaten
-            food.up()  # respawn the food
-            score += 1  # increase score
+        for part in v.snake:
+            part.up()
         
+        # update background if epilepsy mode is enabled
         if epilepsy == 0:
             dispCols = [Back.WHITE, Back.WHITE, Back.WHITE, Back.RESET]
             epilepsy = 1
         elif epilepsy == 1:
             dispCols = [Back.RESET, Back.RESET, Back.RESET, Back.WHITE]
             epilepsy = 0
-        
+
         draw()
         lastTime = time()
 
